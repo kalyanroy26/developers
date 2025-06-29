11fe60e57b4cca7a7d7a308a4cc69aac3af3a7aa
kalyanroy26 <kalyanroy8143@gmail.com>
2025-06-29 13:02:16 +0530

    notes added with some programs

--- Python PS/Day-1/task.py
+++ Python PS/Day-1/task.py
@@ -1,63 +1,40 @@
+# program to print only primes from a given list 
 num_list = [2,3,4,55,72,51,23]
 
-# # prime logic
-# def checkPrime(num):
-#     fact=0
-#     for i in range(2,num):
-#         if num%i==0:
-#             fact+=1
-#             break
-#     if fact==0:
-#         return num
-#     else:
-#         return
+# prime logic
+def checkPrime(num):
+    fact=0
+    for i in range(2,num):
+        if num%i==0:
+            fact+=1
+            break
+    if fact==0:
+        return num
+    else:
+        return
 
-# # execution i bringing each element from list and checking using prime function
-# newList = []
-# for ele in num_list:
-#     newList.append(checkPrime(ele))
+# execution
+newList = []
+for ele in num_list:
+    val = checkPrime(ele)
+    if val is not None: #corrected logic for None error
+        newList.append(val)
 
-# print(newList)
-
-
-
-# problem solving
-
-# 1 strings - string manipulation (index,)
-# palindrome,anagram,reverse,substring,longest substring
-# number logic - number manipulation
-# reverse,palindrome
-# 2 prime,armstrong,perfect,fact,fibb
-#3 list - element,max,min.sec-max,sort(asc,desc), unique elements, duplicates remove
-#4 dict - access 
+print(newList)
 
 
+# program to digits in a number
+num = 526 
+sum = 0 
+while num!=0: 
+    last_digit = num % 10  
+    sum+=last_digit 
+    num = num//10
+print(sum)
 
-
-# number logic
-# 146 - 1 oka character 
-# how to get a char from a number
-
-# num = 526 
-
-# # name = "kalyan" - it is a collection of characters
-# # %
-
-# extraction of a digit - concept
-# sum = 0 
-# while num!=0: 
-#     last_digit = num % 10  
-#     sum+=last_digit 
-#     num = num//10
-# print(sum)
-
-# how you can form a number with a char 1,4,6 - 146
-
-# formation of a number - concept
-
+# program to reverse a number
 num = 721  
 temp = num
-# rev = rev * 10 +last_digit
 rev = 0
 while num!=0:
     last_digit = num%10
@@ -65,17 +42,4 @@ while num!=0:
     num = num//10
 
 print(temp)
-print(rev)
-
-412 
-
-# sum of a number
-# mul of a numer
-# reverse a number
-# palindrome - 121 
-
-num = num //10 
-
-
-
-
+print(rev)   
